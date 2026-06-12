57303ad79c6ff45645b213f8adb6c4c8b3318b39
Artem <arp27849@gmail.com>
2026-06-12 11:39:50 +0600

    Добавил несколько ивентов и сделал функции

--- index.js
+++ index.js
@@ -9,10 +9,41 @@ const bot = mineflayer.createBot({
 })
 
 const events = [
-    'death',
+    'lightning',
     'night',
-    'rain'
+    'apple',
+    'dog',
+
 ]
+
+function lightningEvent() {
+    bot.chat('Event: Гнев зевса')
+
+    bot.chat('/execute as @a at @s run summon minecraft:lightning_bolt ~ ~ ~')
+
+}
+
+function nightEvent() {
+    bot.chat('Event: Солнечное затмение')
+
+    bot.chat('/time set midnight')
+
+}
+
+function appleEvent() {
+    bot.chat('Event: ')
+
+    bot.chat('/give @a minecraft:golden_apple 1')
+}
+
+function dogEvent() {
+    bot.chat('Event: Друг человека')
+    
+
+    bot.chat('/give @a minecraft:bone 5')
+    bot.chat('/execute as @a at @s run summon minecraft:wolf ~2 ~ ~')
+}
+
 
 bot.on('spawn', () => {
     console.log('бот зашел на сервер')
@@ -26,14 +57,21 @@ bot.on('chat', (username, message) => {
     if (message === '!event') {
         const randomEvent = events[Math.floor(Math.random() * events.length)]
 
-        bot.chat(`Выпал ивент: ${randomEvent}`)
 
-        if (randomEvent === 'night') {
-            bot.chat('/time set night')
+        if (randomEvent === 'lightning') {
+            lightningEvent()
         }
 
-        if (randomEvent === 'rain') {
-            bot.chat('/weather rain')
+        if (randomEvent === 'night') {
+            nightEvent()
+        }
+
+        if (randomEvent === 'apple') {
+            appleEvent()
+        }
+
+        if (randomEvent === 'dog') {
+            dogEvent()
         }
     }
 })
